11705535cb49e28d3f5f0448291720ea8deecda9
Phodal Huang <h@phodal.com>
2021-06-20 10:42:11 +0800

    feat: try to set stream

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/antlr/antlr4/runtime/Go/antlr"
 	"github.com/inherd/pling/parser"
 )
@@ -15,18 +14,22 @@ func NewTreeShapeListener() *TreeShapeListener {
 }
 
 func (this *TreeShapeListener) EnterEveryRule(ctx antlr.ParserRuleContext) {
-	fmt.Println(ctx.GetText())
+
 }
 
 func main() {
-	data := "create database hello"
+	data := `CREATE TABLE employee (
+     first_name VARCHAR2(128),
+     last_name VARCHAR2(128),
+     empID NUMBER,
+     salary NUMBER(6) ENCRYPT
+);`
 	input := antlr.NewInputStream(data)
 	lexer := parser.NewPlSqlLexer(input)
 	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
 	p := parser.NewPlSqlParser(stream)
-	fmt.Println(p)
-	//p.AddErrorListener(antlr.NewDiagnosticErrorListener(true))
-	//p.BuildParseTrees = true
-	//tree := p.Json()
-	//antlr.ParseTreeWalkerDefault.Walk(NewTreeShapeListener(), tree)
+	p.AddErrorListener(antlr.NewDiagnosticErrorListener(true))
+	p.BuildParseTrees = true
+	tree := p.Sql_script()
+	antlr.ParseTreeWalkerDefault.Walk(NewTreeShapeListener(), tree)
 }
